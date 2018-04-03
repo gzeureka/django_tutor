bb1787a1366cad2727e2675779628c685346e9e7
gzeureka <gzeureka@gmail.com>
2018-04-03 19:00:30 +0800

    change IndexView.get_queryset to classmethod

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,7 +12,8 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
+    @classmethod
+    def get_queryset(cls):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
